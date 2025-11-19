900dd1713b7289158ec872fd3a69b95d6e37cbcb
mingyanq <1024359684@qq.com>
2025-11-19 10:53:54 +0800

    1.add xcode project files.

---
+++ platform/xcode/elf_parser/elf_parser/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  elf_parser
+//
+//  Created by 秦明岩 on 2025/11/19.
+//
+
+#include <iostream>
+
+int maind1(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
